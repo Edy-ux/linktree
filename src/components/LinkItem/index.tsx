495adcc1c57a7bbee7e07527ee295873395c65a0
fenderlopes@gmail.com <fenderlopes@gmail.com>
2023-02-12 03:32:31 -0300

    feat: update href link for instagram

--- src/components/LinkItem/index.tsx
+++ src/components/LinkItem/index.tsx
@@ -1,11 +1,11 @@
 import { FC } from 'react';
 import { Data } from '../../types';
-import LinkCard from '../LinkCards';
+import LinkCard from '../LinkCard';
 
 const LinkCardItem: FC<Data> = ({ links }) => {
   return (
     <div className="flex flex-col justify-between w-full items-center gap-y-3">
-      {links.map((link, index) => (
+      {links?.map((link, index) => (
         <LinkCard key={index} {...link} />
       ))}
     </div>
